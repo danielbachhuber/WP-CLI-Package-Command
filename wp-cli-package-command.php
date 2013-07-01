aaaf14f0fd30006e2cc63f6987e85c4aec452f4b
Daniel Bachhuber <d@danielbachhuber.com>
2013-06-30 21:24:16 -0700

    Allow community packages to be uninstalled (plus some refactoring)

--- wp-cli-package-command.php
+++ wp-cli-package-command.php
@@ -5,9 +5,11 @@
 use \Composer\Factory;
 use \Composer\IO\NullIO;
 use \Composer\Json\JsonFile;
-use Composer\Package;
+use \Composer\Json\JsonManipulator;
+use \Composer\Package;
 use \Composer\Repository;
 use \Composer\Repository\RepositoryManager;
+use \Composer\Util\Filesystem;
 
 class WP_CLI_Package_Command extends WP_CLI_Command {
 
@@ -57,15 +59,8 @@ class WP_CLI_Package_Command extends WP_CLI_Command {
 		);
 		$assoc_args = array_merge( $defaults, $assoc_args );
 
-		$composer = $this->get_composer();
-		$repo = $composer->getRepositoryManager()->getLocalRepository();
-
 		$packages = array();
-		foreach( $repo->getPackages() as $package ) {
-
-			// WP-CLI community packages always start with 'wp-cli'
-			if ( false === stripos( $package->getName(), '/wp-cli' ) )
-				continue;
+		foreach( $this->get_installed_packages() as $package ) {
 
 			$package_output = new stdClass;
 			$package_output->name = $package->getName();
@@ -81,9 +76,42 @@ class WP_CLI_Package_Command extends WP_CLI_Command {
 	 * Uninstall a WP-CLI community package.
 	 * 
 	 * @subcommand uninstall
+	 * @synopsis <package-name>
 	 */
-	public function uninstall() {
+	public function uninstall( $args ) {
 
+		list( $package_name ) = $args;
+
+		$composer = $this->get_composer();
+		if ( false === ( $package = $this->get_installed_package_by_name( $package_name ) ) )
+			WP_CLI::error( "Package not installed." );
+
+		$composer_json_obj = $this->get_composer_json();
+
+		// Remove the 'require' from composer.json
+		$contents = file_get_contents( $composer_json_obj->getPath() );
+		$manipulator = new JsonManipulator( $contents );
+		$manipulator->removeSubNode( 'require', $package_name );
+		file_put_contents( $composer_json_obj->getPath(), $manipulator->getContents() );
+
+		// Delete the directory
+		$filesystem = new Filesystem;
+		$package_path = getcwd() . '/' . $composer->getConfig()->get('vendor-dir') . '/' . $package->getName();
+		$filesystem->removeDirectory( $package_path );
+
+		// Reset Composer and regenerate the auto-loader
+		$composer = $this->get_composer();
+		$this->regenerate_autoloader();
+
+		WP_CLI::success( "Uninstalled package." );
+	}
+
+	/**
+	 * Check whether a package is a WP-CLI community package
+	 * WP-CLI community packages always start with 'wp-cli'
+	 */
+	private function is_community_package( $package ) {
+		return false !== stripos( $package->getName(), '/wp-cli' );
 	}
 
 	/**
@@ -91,21 +119,93 @@ class WP_CLI_Package_Command extends WP_CLI_Command {
 	 */
 	private function get_composer() {
 
-		$composer_json_path = WP_CLI\Utils\find_file_upward( 'composer.json', WP_CLI_ROOT );
+		$this->set_composer_json_path( WP_CLI\Utils\find_file_upward( 'composer.json', WP_CLI_ROOT ) );
+	
+		// Composer's auto-load generating code makes some assumptions about where
+		// the 'vendor-dir' is, and where Composer is running from.	
+		// Best to just pretend we're installing a package from ~/.composer or similar
+		chdir( pathinfo( $this->get_composer_json_path(), PATHINFO_DIRNAME ) );
+
 		try {
-			$composer = Factory::create( new NullIO, $composer_json_path );
+			$composer = Factory::create( new NullIO, $this->get_composer_json_path() );
 		} catch( Exception $e ) {
 			WP_CLI::error( $e->getMessage() );
 		}
 
-		// Composer expects vendorDir to be in ~/.composer, which isn't always the case
-		// because we still support installing WP-CLI with `git clone`
-		$rm = $composer->getRepositoryManager();
-		$installed_json_path = pathinfo( $composer_json_path, PATHINFO_DIRNAME ) . '/vendor/composer/installed.json';
-		$rm->setLocalRepository( new Repository\InstalledFilesystemRepository( new JsonFile( $installed_json_path ) ) );
-		$composer->setRepositoryManager( $rm );
+		$this->composer = $composer;
+		return $this->composer;
+	}
 
-		return $composer;
+	/**
+	 * Get the installed community packages.
+	 */
+	private function get_installed_packages() {
+
+		$composer = $this->get_composer();
+		$repo = $composer->getRepositoryManager()->getLocalRepository();
+		$installed_packages = array();
+		foreach( $repo->getPackages() as $package ) {
+			if ( ! $this->is_community_package( $package ) )
+				continue;
+			$installed_packages[] = $package;
+		}
+		return $installed_packages;
+	}
+
+	/**
+	 * Get an installed package by its name.
+	 */
+	private function get_installed_package_by_name( $package_name ) {
+		foreach( $this->get_installed_packages() as $package ) {
+			if ( $package_name == $package->getName() )
+				return $package;
+		}
+		return false;
+	}
+
+	/**
+	 * Check if the package name provided is already installed.
+	 */
+	private function is_package_installed( $package_name ) {
+		if ( $this->get_installed_package_by_name( $package_name ) )
+			return true;
+		else
+			return false;
+	}
+
+	/**
+	 * Get the composer.json object
+	 */
+	private function get_composer_json() {
+		return new JsonFile( $this->get_composer_json_path() );
+	}
+
+	/**
+	 * Set the path to composer.json
+	 */
+	private function set_composer_json_path( $path ) {
+		$this->composer_json_path = $path;
+	}
+
+	/**
+	 * Get the path to composer.json
+	 */
+	private function get_composer_json_path() {
+		return $this->composer_json_path;
+	}
+
+	/**
+	 * Regenerate the Composer autoloader
+	 */
+	private function regenerate_autoloader() {
+		$rm = $this->composer->getRepositoryManager();
+		$this->composer->getAutoloadGenerator()->dump(
+			$this->composer->getConfig(),
+			$this->composer->getRepositoryManager()->getLocalRepository(),
+			$this->composer->getPackage(),
+			$this->composer->getInstallationManager(),
+			'composer'
+		);
 	}
 
 }
